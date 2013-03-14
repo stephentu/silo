9c291a737e056e84ac1f7832bf84750c0c5a8562
Stephen Tu <stephentu@csail.mit.edu>
2013-03-14 12:36:53 -0400

    bench data

--- benchmarks/results/make_graphs-3.py
+++ benchmarks/results/make_graphs-3.py
@@ -12,46 +12,52 @@ if __name__ == '__main__':
   for f in files:
     execfile(f)
 
+    benchmarks = set([d[0]['bench'] for d in RESULTS])
+
     # configs
-    descs = [
-      {
-        'x-axis' : 'threads',
-        'y-axis' : lambda x: x[0],
-        'lines' : ['db'], # each line holds this constant
-        'x-label' : 'num threads',
-        'y-label' : 'txns/sec',
-        'title' : 'TPCC throughput graph',
-        'name' : 'throughput',
-      },
-      {
-        'x-axis' : 'threads',
-        'y-axis' : lambda x: x[1],
-        'lines' : ['db'], # each line holds this constant
-        'x-label' : 'num threads',
-        'y-label' : 'aborts/sec',
-        'title' : 'TPCC abort graph',
-        'name' : 'abort',
-      }]
+    def mk_descs(bench):
+      descs = [
+        {
+          'x-axis' : 'threads',
+          'y-axis' : lambda x: x[0],
+          'lines' : ['db'], # each line holds this constant
+          'x-label' : 'num threads',
+          'y-label' : 'txns/sec',
+          'title' : '%s throughput graph' % bench,
+          'name' : 'throughput',
+        },
+        {
+          'x-axis' : 'threads',
+          'y-axis' : lambda x: x[1],
+          'lines' : ['db'], # each line holds this constant
+          'x-label' : 'num threads',
+          'y-label' : 'aborts/sec',
+          'title' : '%s abort graph' % bench,
+          'name' : 'abort',
+        }]
+      return descs
 
-    for desc in descs:
-      lines = {}
-      for (config, result) in RESULTS:
-        key = tuple(config[x] for x in desc['lines'])
-        pts = lines.get(key, {})
-        assert not config[desc['x-axis']] in pts
-        pts[config[desc['x-axis']]] = desc['y-axis'](result)
-        lines[key] = pts
+    for bench in benchmarks:
+      bench_results = [d for d in RESULTS if d[0]['bench'] == bench]
+      for desc in mk_descs(bench):
+        lines = {}
+        for (config, result) in bench_results:
+          key = tuple(config[x] for x in desc['lines'])
+          pts = lines.get(key, {})
+          assert not config[desc['x-axis']] in pts
+          pts[config[desc['x-axis']]] = desc['y-axis'](result)
+          lines[key] = pts
 
-      labels = []
-      for (name, pts) in lines.iteritems():
-        spts = sorted(pts.iteritems(), key=lambda x: x[0])
-        plt.plot([x[0] for x in spts], [x[1] for x in spts])
-        labels.append('-'.join(name))
+        labels = []
+        for (name, pts) in lines.iteritems():
+          spts = sorted(pts.iteritems(), key=lambda x: x[0])
+          plt.plot([x[0] for x in spts], [x[1] for x in spts])
+          labels.append('-'.join(name))
 
-      plt.xlabel(desc['x-label'])
-      plt.ylabel(desc['y-label'])
-      plt.title(desc['title'])
-      plt.legend(labels, loc='upper left')
-      bname = '.'.join(os.path.basename(f).split('.')[:-1])
-      plt.savefig('.'.join([bname + '-' + desc['name'], 'pdf']))
-      plt.close()
+        plt.xlabel(desc['x-label'])
+        plt.ylabel(desc['y-label'])
+        plt.title(desc['title'])
+        plt.legend(labels, loc='upper left')
+        bname = '.'.join(os.path.basename(f).split('.')[:-1])
+        plt.savefig('.'.join([bname + '-' + bench + '-' + desc['name'], 'pdf']))
+        plt.close()
