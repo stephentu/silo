39f15e73878634f4dbcffec4c7958ba4152e7ea3
Stephen Tu <stephentu@csail.mit.edu>
2013-03-18 11:37:50 -0400

    results

--- benchmarks/results/make_graphs-3.py
+++ benchmarks/results/make_graphs-3.py
@@ -14,12 +14,22 @@ if __name__ == '__main__':
 
     benchmarks = set([d[0]['bench'] for d in RESULTS])
 
+    def deal_with_pos0_res(x):
+      if type(x) == list:
+        return [e[0] for e in x]
+      return x[0]
+
+    def deal_with_pos1_res(x):
+      if type(x) == list:
+        return [e[1] for e in x]
+      return x[1]
+
     # configs
     def mk_descs(bench):
       descs = [
         {
           'x-axis' : 'threads',
-          'y-axis' : lambda x: x[0],
+          'y-axis' : deal_with_pos0_res,
           'lines' : ['db'], # each line holds this constant
           'x-label' : 'num threads',
           'y-label' : 'txns/sec',
@@ -28,7 +38,7 @@ if __name__ == '__main__':
         },
         {
           'x-axis' : 'threads',
-          'y-axis' : lambda x: x[1],
+          'y-axis' : deal_with_pos1_res,
           'lines' : ['db'], # each line holds this constant
           'x-label' : 'num threads',
           'y-label' : 'aborts/sec',
@@ -48,10 +58,18 @@ if __name__ == '__main__':
           pts[config[desc['x-axis']]] = desc['y-axis'](result)
           lines[key] = pts
 
+        def mean(x): return sum(x)/len(x)
+        def median(x): return x[len(x)/2]
+
         labels = []
         for (name, pts) in lines.iteritems():
           spts = sorted(pts.iteritems(), key=lambda x: x[0])
-          plt.plot([x[0] for x in spts], [x[1] for x in spts])
+          ypts = [sorted(x[1]) for x in spts]
+          ymins = np.array([min(x) for x in ypts])
+          ymaxs = np.array([max(x) for x in ypts])
+          ymid = np.array([median(x) for x in ypts])
+          yerr=np.array([ymid - ymins, ymaxs - ymid])
+          plt.errorbar([x[0] for x in spts], ymid, yerr=yerr)
           labels.append('-'.join(name))
 
         plt.xlabel(desc['x-label'])
