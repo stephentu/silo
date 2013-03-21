8f502f6db853ab0a02c9c58b32a2c7bc4f8b1e53
Stephen Tu <stephentu@csail.mit.edu>
2013-03-21 12:53:07 -0400

    runner: handle more diverse experiments

--- benchmarks/runner.py
+++ benchmarks/runner.py
@@ -21,28 +21,66 @@ DBS = ('kvdb', 'ndb-proto2')
 THREADS = (1, 4, 8, 12, 16, 20, 24, 28, 32)
 
 #TXN_FLAGS = (0x0, 0x1)
-TXN_FLAGS = (0x1,)
+#TXN_FLAGS = (0x1,)
 
 #SCALE_FACTORS = (10,)
 
 # tuples of (benchname, amplification-factor)
-BENCHMARKS = ( ('ycsb', 1000), ('tpcc', 1), )
+#BENCHMARKS = ( ('ycsb', 1000), ('tpcc', 1), )
 
 NTRIALS = 3
 
-def mk_grid(bench, nthds):
-  # bench is tuple (name, factor)
-  return {
-      'dbs' : DBS,
-      'threads': [nthds],
-      'scale_factors' : [nthds * bench[1]],
-      'benchmarks' : [bench[0]],
-      'txn_flags' : TXN_FLAGS,
-  }
+#def mk_grid(bench, nthds):
+#  # bench is tuple (name, factor)
+#  return {
+#      'dbs' : DBS,
+#      'threads': [nthds],
+#      'scale_factors' : [nthds * bench[1]],
+#      'benchmarks' : [bench[0]],
+#      'txn_flags' : TXN_FLAGS,
+#  }
+#
+#grids = [mk_grid(b, n) for b in BENCHMARKS for n in THREADS]
 
-grids = [mk_grid(b, n) for b in BENCHMARKS for n in THREADS]
+# exp 1:
+#   scale graph: kvdb VS ndb on ycsb 80/20 w/ fixed scale factor 320000
 
-def run_configuration(basedir, dbtype, bench, scale_factor, txn_flags, nthreads):
+grids = [
+  {
+    'dbs' : DBS,
+    'threads' : THREADS,
+    'scale_factors' : [320000],
+    'benchmarks' : ['ycsb'],
+    'bench_opts' : [''],
+    'par_load' : [True],
+  },
+]
+
+# exp 2:
+#   x-axis varies the % multi-partition for new order. hold scale_factor constant @ 28,
+#   nthreads also constant at 28
+D_RANGE = range(0, 11)
+grids += [
+  {
+    'dbs' : ['ndb-proto2'],
+    'threads' : [28],
+    'scale_factors': [28],
+    'benchmarks' : ['tpcc'],
+    'bench_opts' : ['--new-order-remote-item-pct %d' % d for d in D_RANGE],
+    'par_load' : [False],
+  },
+  {
+    'dbs' : ['kvdb'],
+    'threads' : [28],
+    'scale_factors': [28],
+    'benchmarks' : ['tpcc'],
+    'bench_opts' :
+      ['--enable-separate-tree-per-partition --enable-partition-locks --new-order-remote-item-pct %d' % d for d in D_RANGE],
+    'par_load' : [True],
+  },
+]
+
+def run_configuration(basedir, dbtype, bench, scale_factor, nthreads, bench_opts, par_load):
   args = [
       './dbtest',
       '--bench', bench,
@@ -50,9 +88,11 @@ def run_configuration(basedir, dbtype, bench, scale_factor, txn_flags, nthreads)
       '--db-type', dbtype,
       '--num-threads', str(nthreads),
       '--scale-factor', str(scale_factor),
-      '--txn-flags', '%d' % (txn_flags),
+      '--txn-flags', '1',
       '--runtime', '60',
-  ]
+  ] + ([] if not bench_opts else ['--bench-opts', bench_opts]) \
+    + ([] if not par_load else ['--parallel-loading'])
+  print >>sys.stderr, '[INFO] running command %s' % str(args)
   p = subprocess.Popen(args, stdin=open('/dev/null', 'r'), stdout=subprocess.PIPE)
   r = p.stdout.read()
   p.wait()
@@ -66,19 +106,21 @@ if __name__ == '__main__':
   # iterate over all configs
   results = []
   for grid in grids:
-    for (db, bench, scale_factor, txn_flags, threads) in itertools.product(
-        grid['dbs'], grid['benchmarks'], grid['scale_factors'], grid['txn_flags'], grid['threads']):
+    for (db, bench, scale_factor, threads, bench_opts, par_load) in itertools.product(
+        grid['dbs'], grid['benchmarks'], grid['scale_factors'], \
+        grid['threads'], grid['bench_opts'], grid['par_load']):
       config = {
         'db'           : db,
         'bench'        : bench,
         'scale_factor' : scale_factor,
-        'txn_flags'    : txn_flags,
         'threads'      : threads,
+        'bench_opts'   : bench_opts,
+        'par_load'     : par_load
       }
       print >>sys.stderr, '[INFO] running config %s' % (str(config))
       values = []
       for _ in range(NTRIALS):
-        value = run_configuration(basedir, db, bench, scale_factor, txn_flags, threads)
+        value = run_configuration(basedir, db, bench, scale_factor, threads, bench_opts, par_load)
         values.append(value)
       results.append((config, values))
 
