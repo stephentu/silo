91d047d9ea02cbd0788d56d350be5382b251ac67
Stephen Tu <stephentu@csail.mit.edu>
2013-03-25 17:11:01 -0400

    coreid: let core_id() be inlinable

--- core.cc
+++ core.cc
@@ -2,18 +2,6 @@
 #include "unistd.h"
 
 size_t
-coreid::core_id()
-{
-  if (unlikely(tl_core_id == -1)) {
-    // initialize per-core data structures
-    tl_core_id = __sync_fetch_and_add(&g_core_count, 1);
-    // did we exceed max cores?
-    ALWAYS_ASSERT(size_t(tl_core_id) < NMaxCores);
-  }
-  return tl_core_id;
-}
-
-size_t
 coreid::num_cpus_online()
 {
   const long nprocs = sysconf(_SC_NPROCESSORS_ONLN);
--- core.h
+++ core.h
@@ -7,7 +7,19 @@
 class coreid {
 public:
   static const size_t NMaxCores = NMAXCORES;
-  static size_t core_id();
+
+  static inline size_t
+  core_id()
+  {
+    if (unlikely(tl_core_id == -1)) {
+      // initialize per-core data structures
+      tl_core_id = __sync_fetch_and_add(&g_core_count, 1);
+      // did we exceed max cores?
+      ALWAYS_ASSERT(size_t(tl_core_id) < NMaxCores);
+    }
+    return tl_core_id;
+  }
+
   static inline size_t
   core_count()
   {
