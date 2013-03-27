04fc517c1d05f988547b1713eb46660656446cfc
Stephen Tu <stephentu@csail.mit.edu>
2013-03-27 17:24:56 -0400

    runner

--- benchmarks/runner.py
+++ benchmarks/runner.py
@@ -30,62 +30,77 @@ THREADS = (1, 4, 8, 12, 16, 20, 24, 28, 32)
 
 NTRIALS = 3
 
-#def mk_grid(bench, nthds):
-#  # bench is tuple (name, factor)
-#  return {
-#      'dbs' : DBS,
-#      'threads': [nthds],
-#      'scale_factors' : [nthds * bench[1]],
-#      'benchmarks' : [bench[0]],
-#      'txn_flags' : TXN_FLAGS,
-#  }
-#
-#grids = [mk_grid(b, n) for b in BENCHMARKS for n in THREADS]
-
 grids = []
 
 # exp 1:
 #   scale graph: kvdb VS ndb on ycsb 80/20 w/ fixed scale factor 320000
 
-grids += [
-  {
-    'name' : 'scale',
-    'dbs' : DBS,
-    'threads' : THREADS,
-    'scale_factors' : [320000],
-    'benchmarks' : ['ycsb'],
-    'bench_opts' : [''],
-    'par_load' : [True],
-  },
-]
+#grids += [
+#  {
+#    'name' : 'scale',
+#    'dbs' : DBS,
+#    'threads' : THREADS,
+#    'scale_factors' : [320000],
+#    'benchmarks' : ['ycsb'],
+#    'bench_opts' : ['--workload-mix 80,20,0,0'],
+#    'par_load' : [True],
+#    'retry' : [False],
+#  },
+#  {
+#    'name' : 'scale_rmw',
+#    'dbs' : DBS,
+#    'threads' : THREADS,
+#    'scale_factors' : [320000],
+#    'benchmarks' : ['ycsb'],
+#    'bench_opts' : ['--workload-mix 80,0,20,0'],
+#    'par_load' : [True],
+#    'retry' : [False],
+#  },
+#]
 
 # exp 2:
-#   x-axis varies the % multi-partition for new order. hold scale_factor constant @ 28,
-#   nthreads also constant at 28
-D_RANGE = range(0, 11)
-grids += [
-  {
-    'name' : 'multipart:pct',
+def mk_grid(name, bench, nthds):
+  return {
+    'name' : name,
     'dbs' : ['ndb-proto2'],
-    'threads' : [28],
-    'scale_factors': [28],
-    'benchmarks' : ['tpcc'],
-    'bench_opts' : ['--workload-mix 100,0,0,0,0 --new-order-remote-item-pct %d' % d for d in D_RANGE],
+    'threads' : [nthds],
+    'scale_factors' : [nthds],
+    'benchmarks' : [bench],
+    'bench_opts' : [''],
     'par_load' : [False],
-  },
-  {
-    'name' : 'multipart:pct',
-    'dbs' : ['kvdb-st'],
-    'threads' : [28],
-    'scale_factors': [28],
-    'benchmarks' : ['tpcc'],
-    'bench_opts' :
-      ['--workload-mix 100,0,0,0,0 --enable-separate-tree-per-partition --enable-partition-locks --new-order-remote-item-pct %d' % d for d in D_RANGE],
-    'par_load' : [True],
-  },
-]
+    'retry' : [False],
+  }
+grids += [mk_grid('scale_tpcc', 'tpcc', t) for t in THREADS]
 
 # exp 3:
+#   x-axis varies the % multi-partition for new order. hold scale_factor constant @ 28,
+#   nthreads also constant at 28
+#D_RANGE = range(0, 11)
+#grids += [
+#  {
+#    'name' : 'multipart:pct',
+#    'dbs' : ['ndb-proto2'],
+#    'threads' : [28],
+#    'scale_factors': [28],
+#    'benchmarks' : ['tpcc'],
+#    'bench_opts' : ['--workload-mix 100,0,0,0,0 --new-order-remote-item-pct %d' % d for d in D_RANGE],
+#    'par_load' : [False],
+#    'retry' : [False],
+#  },
+#  {
+#    'name' : 'multipart:pct',
+#    'dbs' : ['kvdb-st'],
+#    'threads' : [28],
+#    'scale_factors': [28],
+#    'benchmarks' : ['tpcc'],
+#    'bench_opts' :
+#      ['--workload-mix 100,0,0,0,0 --enable-separate-tree-per-partition --enable-partition-locks --new-order-remote-item-pct %d' % d for d in D_RANGE],
+#    'par_load' : [True],
+#    'retry' : [False],
+#  },
+#]
+
+# exp 4:
 #  * standard workload mix
 #  * fix the tpc-c scale factor at 8
 #  * for volt, do one run @ 8-threads
@@ -99,6 +114,7 @@ grids += [
 #    'benchmarks' : ['tpcc'],
 #    'bench_opts' : ['--enable-separate-tree-per-partition --enable-partition-locks'],
 #    'par_load' : [True],
+#    'retry' : [False],
 #  },
 #  {
 #    'name' : 'multipart:cpu',
@@ -108,11 +124,11 @@ grids += [
 #    'benchmarks' : ['tpcc'],
 #    'bench_opts' : [''],
 #    'par_load' : [False],
+#    'retry' : [False],
 #  },
 #]
 
-
-# exp 4:
+# exp 5:
 #  * 50% new order, 50% stock level
 #  * scale factor 8, n-threads 16
 #  * x-axis is --new-order-remote-item-pct from [0, 20, 40, 60, 80, 100]
@@ -126,6 +142,7 @@ grids += [
     'benchmarks' : ['tpcc'],
     'bench_opts' : ['--workload-mix 50,0,0,0,50 --new-order-remote-item-pct %d' % d for d in RO_DRANGE],
     'par_load' : [False],
+    'retry' : [True],
   },
   {
     'name' : 'readonly',
@@ -135,10 +152,11 @@ grids += [
     'benchmarks' : ['tpcc'],
     'bench_opts' : ['--disable-read-only-snapshots --workload-mix 50,0,0,0,50 --new-order-remote-item-pct %d' % d for d in RO_DRANGE],
     'par_load' : [False],
+    'retry' : [True],
   },
 ]
 
-def run_configuration(basedir, dbtype, bench, scale_factor, nthreads, bench_opts, par_load):
+def run_configuration(basedir, dbtype, bench, scale_factor, nthreads, bench_opts, par_load, retry_aborted_txn):
   args = [
       './dbtest',
       '--bench', bench,
@@ -149,7 +167,8 @@ def run_configuration(basedir, dbtype, bench, scale_factor, nthreads, bench_opts
       '--txn-flags', '1',
       '--runtime', '60',
   ] + ([] if not bench_opts else ['--bench-opts', bench_opts]) \
-    + ([] if not par_load else ['--parallel-loading'])
+    + ([] if not par_load else ['--parallel-loading']) \
+    + ([] if not retry_aborted_txn else ['--retry-aborted-transactions'])
   print >>sys.stderr, '[INFO] running command %s' % str(args)
   p = subprocess.Popen(args, stdin=open('/dev/null', 'r'), stdout=subprocess.PIPE)
   r = p.stdout.read()
@@ -164,9 +183,9 @@ if __name__ == '__main__':
   # iterate over all configs
   results = []
   for grid in grids:
-    for (db, bench, scale_factor, threads, bench_opts, par_load) in itertools.product(
+    for (db, bench, scale_factor, threads, bench_opts, par_load, retry) in itertools.product(
         grid['dbs'], grid['benchmarks'], grid['scale_factors'], \
-        grid['threads'], grid['bench_opts'], grid['par_load']):
+        grid['threads'], grid['bench_opts'], grid['par_load'], grid['retry']):
       config = {
         'name'         : grid['name'],
         'db'           : db,
@@ -174,12 +193,13 @@ if __name__ == '__main__':
         'scale_factor' : scale_factor,
         'threads'      : threads,
         'bench_opts'   : bench_opts,
-        'par_load'     : par_load
+        'par_load'     : par_load,
+        'retry'        : retry,
       }
       print >>sys.stderr, '[INFO] running config %s' % (str(config))
       values = []
       for _ in range(NTRIALS):
-        value = run_configuration(basedir, db, bench, scale_factor, threads, bench_opts, par_load)
+        value = run_configuration(basedir, db, bench, scale_factor, threads, bench_opts, par_load, retry)
         values.append(value)
       results.append((config, values))
 
