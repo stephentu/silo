a4c44d922f5c9ba02f009d0c1a6dbebc2adaa679
Stephen Tu <stephentu@csail.mit.edu>
2013-08-20 19:08:48 -0400

    macros: better padout macro

--- macros.h
+++ macros.h
@@ -51,7 +51,11 @@
 
 // some helpers for cacheline alignment
 #define CACHE_ALIGNED __attribute__((aligned(CACHELINE_SIZE)))
-#define CACHE_PADOUT  char __padout[0] __attribute__((aligned(CACHELINE_SIZE)))
+
+#define __XCONCAT2(a, b) a ## b
+#define __XCONCAT(a, b) __XCONCAT2(a, b)
+#define CACHE_PADOUT  \
+    char __XCONCAT(__padout, __COUNTER__)[0] __attribute__((aligned(CACHELINE_SIZE)))
 #define PACKED __attribute__((packed))
 
 #define NEVER_INLINE  __attribute__((noinline))
